00a4977dd80c5734803c714a2fecebfd9f4f5167
Dang Nguyen Tien Hai <nthdangg@gmail.com>
2026-03-04 22:47:13 +0700

    [dang] interrupt config for user button 0

--- Ex02_BlinkLed/src/main.c
+++ Ex02_BlinkLed/src/main.c
@@ -18,6 +18,15 @@
 #define BTN_PRESSED             0u
 #define BTN_RELEASED            1u
 
+#define S32_NVIC_ISER_BASE  ((uint32_t)0xE000E100u)
+
+typedef struct 
+{
+    volatile uint32_t ISER[15u];
+} S32_NVIC_ISER_t;
+
+S32_NVIC_ISER_t *S32_NVIC_ISER = ((S32_NVIC_ISER_t *)S32_NVIC_ISER_BASE);
+
 void delay_ms(const uint32_t ms)
 {
     volatile uint32_t i = 0u;
@@ -35,26 +44,39 @@ int main(void)
     Wrapper_GPIO_SetDirection(LED_BLUE, ARM_GPIO_OUTPUT);
     
     Wrapper_GPIO_Setup(BTN0, NULL);
+
     Wrapper_GPIO_SetDirection(BTN0, ARM_GPIO_INPUT);
     Wrapper_GPIO_SetPullResistor(BTN0, ARM_GPIO_PULL_UP);
+    
+    /* Interrupt configured PORT_PCR_IRQC */
+    /**
+        1000 ISF flag and Interrupt when logic 0.
+        1001 ISF flag and Interrupt on rising-edge.
+        1010 ISF flag and Interrupt on falling-edge.
+        1011 ISF flag and Interrupt on either edge.
+        1100 ISF flag and Interrupt when logic 1.
+     */
+    IP_PORTC->PCR[BTN0.pin] &= ~PORT_PCR_IRQC_MASK;
+    /* falling edge */
+    IP_PORTC->PCR[BTN0.pin] |= PORT_PCR_IRQC(0b1010);
+
+    /* NVIC enable interrupt */
+    S32_NVIC_ISER->ISER[PORTC_IRQn/32] |= (1 << (PORTC_IRQn % 32));
 
     /* Turn off led */
     Wrapper_GPIO_SetOuput(LED_BLUE, 1u);
 
     while(1)
     {
-        if (BTN_PRESSED == Wrapper_GPIO_GetInput(BTN0))
-        {
-            delay_ms(1000000);
-            if (0 == Wrapper_GPIO_GetDataOuput(LED_BLUE))
-            {
-                Wrapper_GPIO_SetOuput(LED_BLUE, 1);
-            }
-            else
-            {
-                Wrapper_GPIO_SetOuput(LED_BLUE, 0);
-            }
-        }
+        /* Do nothing, wait interrupt */
     }
     return 0;
+}
+
+void PORTC_IRQHandler(void)
+{
+    /* Clear pneding via PORT_PCR_ISF - write 1 to clear*/
+    IP_PORTC->PCR[BTN0.pin] |= PORT_PCR_ISF_MASK;
+    /* Toggle the blue led */
+    Wrapper_GPIO_ToggleOuput(LED_BLUE);
 }
